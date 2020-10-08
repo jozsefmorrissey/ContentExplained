5e4a497b76f2c6259c17f71ddf6bd8c809c46b68
Jozsef Morrissey <jozsef.morrissey@gmail.com>
2020-10-08 17:44:57 -0500

    cleaning removed scripts

--- scripts/t.sh
+++
@@ -1,7 +0,0 @@
-elem='list[0]'
-
-if [[ "$elem" =~ ^[a-zA-Z0-9]*\[[0-9]*\]$ ]]
-then
-  end=${elem##*[}
-  echo ${elem:0:${#elem} - ${#end} - 1}
-fi
--- scripts/watch.sh
+++
@@ -1,2 +0,0 @@
-# find ../html -name '*.html' | entr './build.sh html'
-incrontab /home/jozsef/projects/ContextExplained/html/ IN_MODIFY '/home/jozsef/projects/ContextExplained/scripts/build.sh html'
